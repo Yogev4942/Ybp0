554903ee3059251131b4bf21e43b5de8b420209d
Y H <yogevh16@gmail.com>
2026-01-21 18:20:05 +0200

    fix

---
+++ ybp0/ybp0/Views/EditProfileView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ybp0.Views
+{
+    /// <summary>
+    /// Interaction logic for EditProfileView.xaml
+    /// </summary>
+    public partial class EditProfileView : UserControl
+    {
+        public EditProfileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
